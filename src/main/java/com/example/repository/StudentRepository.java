e128f90cbadf1af6087dab2bdc7edeecfad2c402
Guilherme Freitas <guilhermedin@gmail.com>
2018-08-29 21:36:30 -0300

    ta aparecendo o collection select

---
+++ src/main/java/com/example/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.repository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.model.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+	
+}
